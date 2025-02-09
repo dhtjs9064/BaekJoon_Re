5c24fcb1788e2b78b87f1661dfa548d62787993a
dhtjs9064 <66412758+dhtjs9064@users.noreply.github.com>
2025-02-09 20:22:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 680 ms, Memory: 94012 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
- 
- 
-public class Main {	
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int N = in.nextInt();
-		
-		// list 계열 중 하나를 쓰면 된다.
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		for(int i = 0; i < N; i++) {
-			list.add(in.nextInt());
-		}
-		
-		Collections.sort(list);
-		
-		for(int value : list) {
-			sb.append(value).append('\n');
-		}
-		System.out.println(sb);
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // -1,000,000부터 1,000,000까지의 수 (배열은 음수를 가질 수 없음)
+        boolean[] arr = new boolean[2000001];
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 입력값 처리
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true; // 해당 값의 인덱스를 true로 설정
+        }
+
+        // 0부터 확인하면서 true인 값만 출력 (주어진 범위에 맞게 다시 백만을 뺌)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
 }
