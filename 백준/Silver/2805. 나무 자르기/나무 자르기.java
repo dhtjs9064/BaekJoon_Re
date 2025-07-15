283b36fc52f8202066fd3842e97a026a36f9eff7
dhtjs9064 <66412758+dhtjs9064@users.noreply.github.com>
2025-07-15 16:48:13 +0900

    [Silver II] Title: 나무 자르기, Time: 1656 ms, Memory: 319492 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,27 +1,30 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     // 최선으로 자를 수도 있겠지만 어쩔 수 없이 최악으로 자를 수도 있음 = 이분 탐색 적절
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         int M = sc.nextInt();
+
         int[] tree = new int[N];
 
         // 각각 나무가 10억이 넘어가면 int범위가 초과하므로 long
         // 절단기 최대 길이
         long max = 0;
+
         for (int i = 0; i < N; i++) {
             tree[i] = sc.nextInt();
             if (max < tree[i]) {
                 max = tree[i];
             }
         }
-        
-        //길이 1 방지
-        max++;
+
+        // 나무 길이가 1이어도 절단기는 0이어야 잘리니 필요없음
+/*        max++;*/
 
         //절단기 최소 길이
         long min = 0;
