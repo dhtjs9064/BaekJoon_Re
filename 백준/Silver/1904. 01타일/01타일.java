34faba3d331e04eb31506fc050b8624592ad015b
dhtjs9064 <66412758+dhtjs9064@users.noreply.github.com>
2025-08-19 17:31:24 +0900

    [Silver III] Title: 01타일, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -4,7 +4,39 @@ import java.io.InputStreamReader;
 
 // 참고 : https://st-lab.tistory.com/125
 public class Main {
-    static int[] arr = new int[1000001];
+    // 방법 2
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(Tile(N));
+    }
+
+    static int Tile(int N) {
+
+        int first = 1;
+        int second = 2;
+        int sum = 0;
+
+        if (N == 1) {
+            return first;
+        } else if (N == 2) {
+            return second;
+        } else {
+            for (int i = 2; i < N; i++) {
+                sum = (first + second) % 15746;
+                first = second;
+                second = sum;
+            }
+            return sum;
+        }
+    }
+}
+// 규칙 : N = N-1 + N-2
+
+// 방법 1
+/*    static int[] arr = new int[1000001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,9 +56,8 @@ public class Main {
 
     static int Tile(int N) {
         if (arr[N] == -1) {
+            // 모듈러 공식 사용
             arr[N] = (Tile(N - 1) + Tile(N - 2)) % 15746;
         }
         return arr[N];
-    }
-}
-// 규칙 : N = N-1 + N-2
+    }*/
